35137ff3200918586cccd5dc4cfab3bbcea49ecb
VERDGAE <gaetan.verdin-pol@ext.ec.europa.eu>
2020-02-07 08:08:58 +0100

    add is_unique problem

---
+++ is_unique.py
@@ -0,0 +1,47 @@
+"""
+Is Unique: Implement an algorithm to determine if a string has
+all unique characters.
+What if you cannot use additional data structures?
+"""
+
+import unittest
+
+
+def is_unique(my_string):
+    letter_dict = {}
+
+    for letter in my_string:
+        if letter not in letter_dict:
+            letter_dict[letter] = 1
+        else:
+            letter_dict[letter] += 1
+
+    for letter in my_string:
+        if letter_dict[letter] > 1:
+            return False
+
+    return True
+
+
+# Test
+
+
+class Test(unittest.TestCase):
+    data_true = [("abcd"), ("zea ")]
+    data_false = [("abca"), ("z z")]
+
+    def test_is_uniqeu(self):
+
+        # true res
+        for test_string in self.data_true:
+            res = is_unique(test_string)
+            self.assertTrue(res)
+
+        # false res
+        for test_string in self.data_false:
+            res = is_unique(test_string)
+            self.assertFalse(res)
+
+
+if __name__ == "__main__":
+    unittest.main()
